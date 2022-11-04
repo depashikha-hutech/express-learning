25355956736f4c09b7a3d6ebf6b2a5ecf722fd71
DipshikhaMahapatra <depashikha@hutechsolutions.com>
2022-11-04 17:50:08 +0530

    4/11/2022
     express routing ,jwt theory

--- expressbasic.js
+++ expressbasic.js
@@ -57,3 +57,76 @@ app.get(/.*fly$/, (req, res) => {
   app.get('/flights/:from-:to', (req, res) => {
     res.send(req.params)
   })
+  //route handlers
+ // A single callback function can handle a route. 
+
+app.get('/example/a', (req, res) => {
+  res.send('Hello from A!')
+})
+//More than one callback function can handle the next object.
+
+app.get('/hii/k', (req, res, next) => {
+  console.log('the response will be sent by the next function ...')
+  next()
+}, (req, res) => {
+  res.send('Hello from B!')
+})
+//An array of callback functions can handle a route. For example:
+
+const cb0 = function (req, res, next) {
+  console.log('CB0')
+  next()
+}
+
+const cb1 = function (req, res, next) {
+  console.log('CB1')
+  next()
+}
+
+const cb2 = function (req, res) {
+  res.send('Hello from C!')
+}
+
+app.get('/hellohi/c', [cb0, cb1, cb2])
+// combination of independent functions and arrays of functions can handle a route. For example:
+
+const h0 = function (req, res, next) {
+  console.log('H0')
+  next()
+}
+
+const h1 = function (req, res, next) {
+  console.log('H1')
+  next()
+}
+
+app.get('/exmple/d', [h0, h1], (req, res, next) => {
+  console.log('the response will be sent by the next function ...')
+  next()
+}, (req, res) => {
+  res.send('Hello from D!')
+})
+//
+app.get('/exmple/json', (req, res) =>{
+ // res.jsonp({ user: 'tobi' })
+ res.status(500).jsonp({ error: 'message' })
+})
+
+// an example of chained route handlers that are defined by using app.route().
+
+app.route('/book')
+.get((req, res) => {
+  res.send('Get a random book')
+})
+.post((req, res) => {
+  res.send('Add a book')
+})
+.put((req, res) => {
+  res.send('Update the book')
+})
+// express .route
+const birds = require('./birds')
+
+app.use('/birds', birds)
+
+
